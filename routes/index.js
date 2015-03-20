d1563e6a14f71e02c695f68c6692307179cf416f
Ignacio Rivas <rivasign@gmail.com>
2015-03-20 12:07:26 +0000

    Add styling and static templating

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,17 @@
+var models  = require('../models');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {
-    title: 'Express',
-    users: undefined
-  });
+  models.Movie.findAll({
+        include: [ models.Torrent ]
+      }).then(function(movies) {
+        res.render('index', {
+          title: 'page title',
+          movies: movies
+        });
+      });
 });
 
 module.exports = router;
