e92fc3e2956c1d3a92ae490c8eb15c6cbc1686d5
Ignacio Rivas <rivasign@gmail.com>
2015-03-20 21:26:34 +0000

    Make worker store movie data

--- worker/index.js
+++ worker/index.js
@@ -1,7 +1,7 @@
 var request = require('request');
 var _ = require('lodash');
 var Promise = require('bluebird');
-var models  = require('./models');
+var models  = require('../models');
 
 
 var fetchMovie = function(url, options) {
@@ -10,6 +10,7 @@ var fetchMovie = function(url, options) {
       if (!err && res.statusCode === 200) {
         resolve(JSON.parse(body));
       } else {
+        console.log(res.statusCode);
         reject(err);
       }
     });
@@ -60,9 +61,9 @@ var saveDownloads = function(movie) {
 
 var fetchOneMovie = function(page) {
   return new Promise(function(resolve, reject) {
-    fetchMovie('https://yts.re/api/v2/list_movies.json', {
+    fetchMovie('https://yts.im/api/v2/list_movies.json', {
       minimum_rating: 5,
-      limit: 10,
+      limit: 20,
       page: page
     }).then(function(res) {
 
@@ -94,24 +95,129 @@ var fetchOneMovie = function(page) {
 };
 
 
-var fetchOneMovieDetail = function(id) {
+
+var getMovieById = function(id) {
   return new Promise(function(resolve, reject) {
-    fetchMovie('https://yts.re/api/v2/movie_details.json', {
-      movie_id: id,
-      with_images: true,
-      with_cast: true
-    }).then(function(res) {
-      console.log('Details fetched for movie => ' + res.data.title_long);
-      resolve();
+
+    models.Movie.find({
+      where: { _id: id }
+    }).then(function(movie) {
+      resolve(movie);
     }).catch(function(err) {
-      console.log(err);
       reject(err);
     });
   });
 };
 
 
-var fetchFromPages = [1, 2, 3, 4, 5, 6, 7, 8 ,9, 10, 11, 12, 13, 14, 15];
+var storeMovieDetails = function(response) {
+  return new Promise(function(resolve, reject) {
+    var data = _.pick(response.data, 'yt_trailer_code', 'description_intro', 'description_full');
+    data._id = response.data.id;
+
+    models.MovieDetails.create(data).then(function(details) {
+
+      getMovieById(response.data.id).then(function(movie) {
+        details.setMovie(movie).then(function() {
+          resolve(_.extend(response, {
+            detailsObject: details
+          }));
+        });
+      });
+
+    }).catch(function(err) {
+      reject(err);
+    });
+  });
+};
+
+
+var storeMovieArtwork = function(response) {
+  return new Promise(function(resolve, reject) {
+    models.MovieArtwork.create(response.data.images).then(function(artwork) {
+      artwork.setMovieDetail(response.detailsObject).then(function(m) {
+        resolve(response);
+      }).catch(function(err) {
+        reject(err);
+      });
+
+    }).catch(function(err) {
+      reject(err);
+    });
+  });
+};
+
+
+var storeMovieDirectors = function(response) {
+  return Promise.map(response.data.directors, function(director) {
+      return models.Director.create(director).then(function(directorObject) {
+        directorObject.setMovieDetail(response.detailsObject).then(function(d) {
+          return response;
+        }).catch(function(err) {
+          console.log('ERROR seting movie detail');
+          return err;
+        });
+      }).catch(function(err) {
+        console.log('ERROR creating director');
+        return err
+      });
+    }).then(function(response) {
+      return response;
+    }).catch(function(err) {
+      console.log('ERROR ADDING DIRECTOR');
+      return err;
+    });
+};
+
+var storeMovieActors = function(response) {
+  return new Promise(function(resolve, reject) {
+    Promise.map(response.data.actors, function(actor) {
+      var tmp = _.omit(actor, 'character_name');
+      tmp.character = actor.character_name;
+
+      return models.Actor.create(tmp).then(function(actorObject) {
+        actorObject.setMovieDetail(response.detailsObject).then(function(d) {
+          return response;
+        }).catch(function(err) {
+          console.log('ERROR seting movie detail');
+          return err
+        });
+      }).catch(function(err) {
+        console.log('ERROR creating actor');
+        return err
+      });
+    }).then(function(response) {
+      resolve(response);
+    }).catch(function(err) {
+      console.log('ERROR ADDING ACTOR');
+      reject(err);
+    });
+  });
+};
+
+
+var fetchOneMovieDetail = function(id) {
+  return new Promise(function(resolve, reject) {
+    fetchMovie('https://yts.im/api/v2/movie_details.json', {
+      movie_id: id,
+      with_images: true,
+      with_cast: true
+    }).then(storeMovieDetails)
+    .then(storeMovieArtwork)
+    .then(storeMovieDirectors)
+    .then(storeMovieActors)
+    .then(function(res) {
+      console.log('Details stored for movie => ' + res.data.title_long);
+      resolve();
+    }).catch(function(err, e, s) {
+      console.log('Error storing a movie details');
+      resolve();
+    });
+  });
+};
+
+
+var fetchFromPages = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 Promise.map(fetchFromPages, function(page) {
   return fetchOneMovie(page);
