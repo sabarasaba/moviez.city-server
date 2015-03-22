8d193ef6f9e09e1e6664ea345a5c4c1635db8a72
Ignacio Rivas <rivasign@gmail.com>
2015-03-22 10:32:25 +0000

    Add pagination for movies.get

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,14 +1,29 @@
 import models from '../models';
 import express from 'express';
+import _ from 'lodash';
 
 let router = express.Router();
 
+const defaults = {
+  limit: 10,
+  page: 1
+};
+
 router.get('/', (req, res) => {
-  models.Movie.findAll({
-        include: [ models.Torrent ]
+  let params = _.defaults(req.query, defaults);
+
+  models.Movie.findAndCountAll({
+        include: [ models.Torrent ],
+        offset: (params.limit * params.page) - params.limit,
+        limit: params.limit
       }).then(movies => {
         res.json({
-          movies: movies
+          data: {
+            total: movies.count,
+            page: params.page,
+            limit: params.limit
+          },
+          movies: movies.rows
         });
       });
 });
