368015f2e8f3f1d908fbb1b810bf54e2814bf3d5
Ignacio Rivas <rivasign@gmail.com>
2015-11-10 21:39:22 +0000

    Fix categories response

--- src/controllers/categories.js
+++ src/controllers/categories.js
@@ -1,14 +1,23 @@
 import models from '../models';
 import express from 'express';
+import _ from 'lodash';
 
 let router = express.Router();
 
 router.get('/', (req, res) => {
   models.sequelize.query('SELECT DISTINCT name FROM "Categories" ORDER BY name ASC', { type: models.sequelize.QueryTypes.SELECT})
     .then((categories) => {
+      const data = _.map(categories, (e, key) => {
+        e.id = key;
+
+        return e;
+      });
+
       res.json({
-        total: categories.length,
-        categories: categories
+        meta: {
+          total: categories.length,
+        },
+        categories: data
       });
     });
 });
