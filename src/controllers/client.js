a2ce6c7c305d5cf42f1fb916368d51c244aa66c5
Ignacio Rivas <rivasign@gmail.com>
2015-11-11 21:24:49 +0000

    Update client app and always render app

--- src/controllers/client.js
+++ src/controllers/client.js
@@ -4,7 +4,7 @@ import path from 'path';
 let router = express.Router();
 
 router.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public', 'index.html'));
+  res.sendFile(path.join(__dirname, '../public', 'index2.html'));
 });
 
 export default router;
